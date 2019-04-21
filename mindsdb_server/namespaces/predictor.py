6a6f5ea60b93b769fb087d96e3565b5a8aa821d9
George3d6 <george@cerebralab.com>
2019-04-21 19:15:41 +0000

    that was some heroic debugging if I do say so myself

--- mindsdb_server/namespaces/predictor.py
+++ mindsdb_server/namespaces/predictor.py
@@ -2,14 +2,31 @@ from flask_restplus import Resource, fields
 
 from mindsdb_server.namespaces.entitites.predictor_status import predictor_status, EXAMPLES as PREDICTORS_STATUS_LIST
 from mindsdb_server.namespaces.entitites.predictor_metadata import predictor_metadata, EXAMPLES as PREDICTOR_METADATA
-
 from mindsdb_server.namespaces.configs.predictors import ns_conf
+
+import mindsdb
+
 import json
 import pickle
 import sys
-import mindsdb
+import copy
 from dateutil.parser import parse as parse_datetime
 
+
+def debug_pkey_type(model, keys=None, reset_keyes=True, type_to_check=list, append_key=True):
+    if type(model) != dict:
+        return
+    for k in model:
+        if reset_keyes:
+            keys = []
+        if type(model[k]) == dict:
+            keys.append(k)
+            debug_pkey_type(model[k], copy.deepcopy(keys), reset_keyes=False)
+        if type(model[k]) == type_to_check:
+            print(f'They key {keys}->{k} has type list')
+        if type(model[k]) == list:
+            for item in model[k]:
+                debug_pkey_type(item, copy.deepcopy(keys), reset_keyes=False)
 
 @ns_conf.route('/')
 class PredictorList(Resource):
@@ -41,5 +58,10 @@ class Predictor(Resource):
         for k in ['train_end_at', 'updated_at', 'created_at']:
             if k in model:
                 model[k] = parse_datetime(model[k])
+
+        #debug_pkey_type(model)
+        #print(model['data_analysis']['target_columns_metadata'])
+        #model['data_analysis']['target_columns_metadata'] = 0
+        #model['data_analysis']['input_columns_metadata'] = 0
 
         return model
