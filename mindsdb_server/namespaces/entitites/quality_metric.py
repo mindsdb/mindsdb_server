16c9315fcaed5c16ff4fb8d4dcd73145a94cc0e2
George3d6 <george@cerebralab.com>
2019-05-23 23:17:15 +0000

    changed quality metrics schema

--- mindsdb_server/namespaces/entitites/quality_metric.py
+++ mindsdb_server/namespaces/entitites/quality_metric.py
@@ -5,5 +5,7 @@ from flask_restplus import fields
 quality_metric = ns_conf.model('QualityMetric', {
     'type': fields.String(required=False, description='The quality type', enum=['error', 'warning', 'info']),
     'score': fields.Float(required=False, description='The score on the specific metric value 0-1'),
-    'description': fields.String(required=False, description='The quality metric description')
+    'description': fields.String(required=False, description='The quality metric description'),
+    'warning': fields.String(required=False, description=''),
+    'name': fields.String(required=False, description=''),
 })
