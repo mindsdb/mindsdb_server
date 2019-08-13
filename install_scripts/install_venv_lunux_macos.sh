e6b80536edbf2a1376eb8cb703947959d33a3841
George3d6 <george@cerebralab.com>
2019-08-13 18:21:41 +0000

    added install scripts

---
+++ install_scripts/install_venv_lunux_macos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+"$PYPATH" -m pip install --user --upgrade pip
+"$PYPATH" -m pip install --user virtualenv
+
+mkdir "$SERVER_PATH"
+cd "$SERVER_PATH"
+"$PYPATH" -m virtualenv env
+source env/bin/activate
+env/bin/pip install --upgrade pip
+env/bin/pip install mindsdb --no-cache-dir 
+env/bin/pip install mindsdb-server --no-cache-dir
+echo "
+from mindsdb_server import start_server
+if __name__ == '__main__':
+    start_server()
+" > run_mindsdb_server.py
---
+++ install_scripts/install_venv_win.bat
@@ -0,0 +1,14 @@
+"%PYPATH%" -m pip install --user --upgrade pip
+"%PYPATH%" -m pip install --user virtualenv
+
+mkdir "%SERVER_PATH%"
+cd "%SERVER_PATH%"
+"%PYPATH%" -m virtualenv env
+
+env\Scripts\pip.exe install --upgrade pip
+env\Scripts\pip.exe install git+https://github.com/mindsdb/mindsdb.git@master
+env\Scripts\pip.exe install mindsdb-server --no-cache-dir
+
+echo from mindsdb_server import start_server > run_mindsdb_server.py
+echo if __name__ == '__main__': >> run_mindsdb_server.py
+echo     start_server() >> run_mindsdb_server.py
