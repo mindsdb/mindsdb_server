fa796e0b9ed5d13e34099d4931c9a43a9aad0581
George3d6 <george@cerebralab.com>
2019-08-20 14:13:39 +0000

    updated install req to 3.6

--- mindsdb_server/__init__.py
+++ mindsdb_server/__init__.py
@@ -1,6 +1,6 @@
 import sys
-if sys.version_info < (3,3):
-    sys.exit('Sorry, For MindsDB Server does not support python < 3.3')
+if sys.version_info < (3,6):
+    sys.exit('Sorry, For MindsDB Server does not support python < 3.6')
 
 
 from mindsdb_server.server import start_server
