2fe765b9ce82b02b83dc4d546aeaf15f39db8f6a
Jorge Torres <jorge.torres.maldonado@gmail.com>
2020-01-20 16:43:51 -0800

    Update install_venv_win.bat

--- install_scripts/install_venv_win.bat
+++ install_scripts/install_venv_win.bat
@@ -7,6 +7,7 @@ cd "%SERVER_PATH%"
 
 env\Scripts\pip.exe install --upgrade pip
 env\Scripts\pip.exe install git+https://github.com/mindsdb/mindsdb.git@master
+env\Scripts\pip.exe install torch===1.4.0 torchvision===0.5.0 -f https://download.pytorch.org/whl/torch_stable.html 
 env\Scripts\pip.exe install mindsdb-server --no-cache-dir
 
 echo from mindsdb_server import start_server > run_mindsdb_server.py
