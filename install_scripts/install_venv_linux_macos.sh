e0cb13031cccaee8b1768f9b52b90e1ba54826cc
George3d6 <george@cerebralab.com>
2020-01-21 16:54:51 +0200

    added reinstalling to scritps

--- install_scripts/install_venv_linux_macos.sh
+++ install_scripts/install_venv_linux_macos.sh
@@ -8,7 +8,7 @@ cd "$SERVER_PATH"
 "$PYPATH" -m virtualenv env
 source env/bin/activate
 env/bin/pip install --upgrade pip
-env/bin/pip install mindsdb --no-cache-dir 
+env/bin/pip install mindsdb --upgrade --force-reinstall --no-cache-dir
 env/bin/pip install mindsdb-server --no-cache-dir
 echo "
 from mindsdb_server import start_server
--- install_scripts/install_venv_win.bat
+++ install_scripts/install_venv_win.bat
@@ -6,8 +6,7 @@ cd "%SERVER_PATH%"
 "%PYPATH%" -m virtualenv env
 
 env\Scripts\pip.exe install --upgrade pip
-env\Scripts\pip.exe install git+https://github.com/mindsdb/mindsdb.git@master
-env\Scripts\pip.exe install torch===1.4.0 torchvision===0.5.0 -f https://download.pytorch.org/whl/torch_stable.html 
+env\Scripts\pip.exe install git+https://github.com/mindsdb/mindsdb.git@master --upgrade --force-reinstall --no-cache-dir
 env\Scripts\pip.exe install mindsdb-server --no-cache-dir
 
 echo from mindsdb_server import start_server > run_mindsdb_server.py
