c814c85e2cf34540bd35e8b55cb8b91bd6f8da50
George3d6 <george@cerebralab.com>
2020-02-12 17:22:07 +0200

    fuirther update to install script

--- install_scripts/install_venv_linux_macos.sh
+++ install_scripts/install_venv_linux_macos.sh
@@ -7,9 +7,9 @@ mkdir "$SERVER_PATH"
 cd "$SERVER_PATH"
 "$PYPATH" -m virtualenv env --python="$PYPATH"
 source env/bin/activate
-pip install --upgrade pip || pip3 install --upgrade pip
-pip install mindsdb --upgrade --no-cache-dir || pip3 install mindsdb --upgrade --no-cache-dir
-pip install mindsdb-server --upgrade --no-cache-dir || pip3 install mindsdb-server --upgrade --no-cache-dir
+env/bin/pip3 install --upgrade pip || env/bin/pip install --upgrade pip
+env/bin/pip3 install mindsdb --upgrade --no-cache-dir || env/bin/pip install mindsdb --upgrade --no-cache-dir
+env/bin/pip3 install mindsdb-server --upgrade --no-cache-dir || env/bin/pip install mindsdb-server --upgrade --no-cache-dir
 echo "
 from mindsdb_server import start_server
 if __name__ == '__main__':
