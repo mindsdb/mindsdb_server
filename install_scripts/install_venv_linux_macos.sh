bebea9e714c979f7acbbe8bbaf2418a16655b07b
George3d6 <george@cerebralab.com>
2020-02-18 09:19:18 +0200

    updated windows intaller to use pip, updated linux installer not to upgrade, added separate upgrade script

--- install_scripts/install_venv_linux_macos.sh
+++ install_scripts/install_venv_linux_macos.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-"$PYPATH" -m pip install --user --upgrade pip
-"$PYPATH" -m pip install --user virtualenv
-
-mkdir -p "$SERVER_PATH" || mkdir "$SERVER_PATH"
 cd "$SERVER_PATH"
-"$PYPATH" -m virtualenv env --python="$PYPATH"
 source env/bin/activate
-env/bin/pip3 install --upgrade pip || env/bin/pip install --upgrade pip
-env/bin/pip3 install mindsdb --upgrade --no-cache-dir || env/bin/pip install mindsdb --upgrade --no-cache-dir
-env/bin/pip3 install mindsdb-server --upgrade --no-cache-dir || env/bin/pip install mindsdb-server --upgrade --no-cache-dir
+env/bin/pip3 install  pip || env/bin/pip install  pip
+env/bin/pip3 install mindsdb  --no-cache-dir || env/bin/pip install mindsdb  --no-cache-dir
+env/bin/pip3 install mindsdb-server  --no-cache-dir || env/bin/pip install mindsdb-server  --no-cache-dir
 echo "
 from mindsdb_server import start_server
 if __name__ == '__main__':
