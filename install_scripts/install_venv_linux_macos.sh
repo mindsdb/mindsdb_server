8ee0661ba2e37dfaebac804133f330e3cadc5f60
George3d6 <george@cerebralab.com>
2020-02-18 19:44:48 +0200

    trying another install script

--- install_scripts/install_venv_linux_macos.sh
+++ install_scripts/install_venv_linux_macos.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-"$PYPATH" -m pip install --user --upgrade pip
-"$PYPATH" -m pip install --user virtualenv
-mkdir -p "$SERVER_PATH" || mkdir "$SERVER_PATH"
+"$PYPATH" -m pip install --user --upgrade pip;
+"$PYPATH" -m pip install --user virtualenv;
+mkdir -p "$SERVER_PATH" || mkdir "$SERVER_PATH";
 
-cd "$SERVER_PATH"
+cd "$SERVER_PATH";
 
-"$PYPATH" -m virtualenv env --python="$PYPATH"
+"$PYPATH" -m virtualenv env --python="$PYPATH";
 
-source ""$SERVER_PATH"/env/bin/activate"
-""$SERVER_PATH"/env/bin/pip3" install --upgrade pip || ""$SERVER_PATH"/env/bin/pip" install --upgrade pip
-""$SERVER_PATH"/env/bin/pip3" install mindsdb  --no-cache-dir || ""$SERVER_PATH"/env/bin/pip" install mindsdb  --no-cache-dir
-""$SERVER_PATH"/env/bin/pip3" install mindsdb-server  --no-cache-dir || ""$SERVER_PATH"/env/bin/pip" install mindsdb-server  --no-cache-dir
+source "./"$SERVER_PATH"/env/bin/activate";
+
+echo "./"$SERVER_PATH"/env/bin/activate";
+
+"./"$SERVER_PATH"/env/bin/pip3" install --upgrade pip || "./"$SERVER_PATH"/env/bin/pip" install --upgrade pip
+"./"$SERVER_PATH"/env/bin/pip3" install mindsdb  --no-cache-dir || "./"$SERVER_PATH"/env/bin/pip" install mindsdb  --no-cache-dir
+"./"$SERVER_PATH"/env/bin/pip3" install mindsdb-server  --no-cache-dir || "./"$SERVER_PATH"/env/bin/pip" install mindsdb-server  --no-cache-dir
 echo "
 from mindsdb_server import start_server
 if __name__ == '__main__':
     start_server()
-" > run_mindsdb_server.py
+" > run_mindsdb_server.py;
