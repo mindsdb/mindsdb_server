d2484138ff09b9103c7c7282e3adeb8d893f8a73
Zoran Pandovski <zoran.pandovski@keitaro.com>
2020-02-18 16:49:51 +0100

    Minor twiks

--- mindsdb_server/namespaces/entitites/label_group.py
+++ mindsdb_server/namespaces/entitites/label_group.py
@@ -1,6 +1,7 @@
 from mindsdb_server.namespaces.configs.predictors import ns_conf
 from flask_restx import fields
 
+
 label_group = ns_conf.model('LabelGroup', {
     'group': fields.String(required=False, description='label name'),
     'members': fields.List(fields.String, required=False, description='members belonging to this group'),
