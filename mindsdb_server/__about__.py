a3cde8290f926d54669ac0a818aab1d595caa58b
George3d6 <george@cerebralab.com>
2020-05-22 03:12:54 +0300

    set version

--- mindsdb_server/__about__.py
+++ mindsdb_server/__about__.py
@@ -1,10 +1,6 @@
 __title__ = 'mindsdb_server'
 __package_name__ = 'mindsdb_server'
-<<<<<<< HEAD
-__version__ = '1.9.1'
-=======
-__version__ = '1.9.3'
->>>>>>> 7baa7cf817e3c4b95e12c6ae8041d2aa3beea411
+__version__ = '1.9.4'
 __description__ = "MindsDB server, provides server capabilities to mindsdb native python library"
 __email__ = "jorge@mindsdb.com"
 __author__ = 'MindsDB Inc'
