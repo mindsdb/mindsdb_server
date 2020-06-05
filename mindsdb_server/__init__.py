cef08f190a6b99353320f6c65859c79081399d94
George3d6 <george@cerebralab.com>
2020-06-05 03:45:16 +0300

    kinda working

--- mindsdb_server/__init__.py
+++ mindsdb_server/__init__.py
@@ -1,6 +1,7 @@
 # Change the way these are imported depending on how they are installed in the future
 
 from mindsdb import *
+# Figure out how to add this as a module
 import lightwood
 #import dataskillet
 print(f'Initialized under name {__name__}')
