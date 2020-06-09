41a610a20f781140e1caa64acda4d11e82ec0b6b
Max Stepanov <stpmax@yandex.ru>
2020-06-09 11:02:13 +0000

    removed config parse

--- mindsdb_server/api/mysql/start.py
+++ mindsdb_server/api/mysql/start.py
@@ -4,11 +4,6 @@ from mindsdb_server.utilities.config import config
 from mindsdb_server.api.mysql.mysql_proxy.mysql_proxy import MysqlProxy
 
 def start():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--config', type=str, default='/etc/mindsdb/config.json')
-    args = parser.parse_args()
-    config.merge(args.config)
-
     MysqlProxy.startProxy()
 
 if __name__ == '__main__':
