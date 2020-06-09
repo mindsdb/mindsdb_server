665285ae6c775d4cea8aa8d663880ede2466bb94
George3d6 <george@cerebralab.com>
2020-06-09 13:34:01 +0300

    finished clickhouse interface and hooks for model integration

--- mindsdb_server/interfaces/clickhouse/clickhouse.py
+++ mindsdb_server/interfaces/clickhouse/clickhouse.py
@@ -1,4 +1,5 @@
 import requests
+from mindsdb.libs.constants.mindsdb import DATA_TYPES, DATA_SUBTYPES
 
 
 class Clickhouse():
@@ -10,8 +11,38 @@ class Clickhouse():
         self.password = config['interface']['clickhouse']['password']
 
         self.type_mapping = {
-            
+
         }
+
+
+    def _to_clickhouse_table(self, stats):
+        subtype_map = {
+            DATA_SUBTYPES.INT: 'Int64',
+            DATA_SUBTYPES.FLOAT: 'Float64',
+            DATA_SUBTYPES.BINARY: 'UInt8',
+            DATA_SUBTYPES.DATE: 'Date',
+            DATA_SUBTYPES.TIMESTAMP: 'Datetime',
+            DATA_SUBTYPES.SINGLE: 'String',
+            DATA_SUBTYPES.MULTIPLE: 'String',
+            DATA_SUBTYPES.IMAGE: 'String',
+            DATA_SUBTYPES.VIDEO: 'String',
+            DATA_SUBTYPES.AUDIO: 'String'
+            DATA_SUBTYPES.TEXT: 'String',
+            DATA_SUBTYPES.ARRAY: 'Array(Float64)'
+        }
+
+        column_declaration = []
+        for column in stats:
+            try:
+                name = column['name']
+                col_subtype = stats[name]['typing']['data_subtype']
+                new_type = subtype_map[col_subtype]
+                column_declaration.append(f' {name} {col_subtype} ')
+            except Exception as e:
+                print(e)
+                print(f'Error: cant convert type {col_subtype} of column {name} to clickhouse tpye')
+
+        return column_declaration
 
     def _query(self, query):
         params = {'user': user}
@@ -22,9 +53,17 @@ class Clickhouse():
 
         return response
 
-    def sync_predictor(self, name):
-        self._query(f"""
+    def register_predictor(self, name, stats):
+        columns_sql = '\n'.join(self._to_clickhouse_table(stats))
+
+        msqyl_conn =  self.config['api']['mysql']['host'] + ':' + str(self.config['api']['mysql']['port'])
+        msqyl_user =  self.config['api']['mysql']['user']
+        msqyl_pass =  self.config['api']['mysql']['password']
+
+        q = f"""
                 CREATE TABLE {name}
-                (rental_price String , initial_price String)
-                ENGINE=MySQL('127.0.0.1:3306', 'minds', 'home_rentals', 'root', 'test1')
-                    """)
+                ({columns_sql}
+                ) ENGINE=MySQL('{msqyl_conn}', 'minds', '{name}', '{msqyl_user}', '{msqyl_pass}')
+        """
+        print(f'Executing table creation query to sync predictor:\n{q}\n')
+        self._query(q)
--- mindsdb_server/interfaces/datastore/sqlite_helpers.py
+++ mindsdb_server/interfaces/datastore/sqlite_helpers.py
@@ -1,4 +1,5 @@
 import sqlite3
+from mindsdb.libs.constants.mindsdb import DATA_TYPES, DATA_SUBTYPES
 
 
 def create_sqlite_db(path, data_frame):
