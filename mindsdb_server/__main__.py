3ec907f991758535ea39ec1aa1cb308a43c561f5
George3d6 <george@cerebralab.com>
2020-06-09 13:00:09 +0300

    switched to multiprocessing

--- mindsdb_server/__main__.py
+++ mindsdb_server/__main__.py
@@ -5,43 +5,33 @@ from multiprocessing import Pool, Process
 import os
 import traceback
 
-print(f'Main call under name {__name__}')
-
 def close_api_gracefully(pool):
-    print('Shutting down !')
     pool.terminate()
     pool.join()
 
 parser = argparse.ArgumentParser(description='CL argument for mindsdb server')
-parser.add_argument('--api', type=str, default='http,mysql') # alternative when mysql api is ready: default='http,mysql'
+parser.add_argument('--api', type=str, default='http,mysql')
 parser.add_argument('--config', type=str, default='/etc/mindsdb/config.json')
 
 args = parser.parse_args()
 api_arr = args.api.split(',')
-pool = Pool(processes=len(api_arr))
-
+allowed_apis = ['http','mysql']
 p_arr = []
 
 for api in api_arr:
-    print(api_arr)
-    print(f'\n\n\n{api}\n\n\n')
-    print(f'Starting Mindsdb {api} API !')
+    if api not in allowed_apis:
+        raise Exception(f'API {api} not in the list of allowed apis: {allowed_apis}')
     try:
         start = importlib.import_module(f'mindsdb_server.api.{api}.start')
         p = Process(target=start.start)
         p.start()
         p_arr.append(p)
-        print(f'Started Mindsdb {api} API ! <clap, clap, clap>')
+        print(f'Started Mindsdb {api} API !')
     except Exception as e:
-        #close_api_gracefully(pool)
-        print(f'Failed to start {api} API with exception {e}')
         print(traceback.format_exc())
-        #exit()
+        print(f'Failed to start {api} API with exception {e}')
 
 for p in p_arr:
-    print(p)
-    import time
-    time.sleep(40)
     p.join()
 
 atexit.register(close_api_gracefully, pool=pool)
