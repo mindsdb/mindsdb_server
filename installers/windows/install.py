397b8d8b12a087bb2a56b3c50e3969645055a818
maximlopin <wzzhz13@gmail.com>
2020-06-18 15:10:17 +0300

    install vc_redist

--- installers/windows/install.py
+++ installers/windows/install.py
@@ -7,6 +7,7 @@ assert os.name == 'nt'
 
 PY_EMBED_URL = 'https://www.python.org/ftp/python/3.7.4/python-3.7.4-embed-amd64.zip'
 GET_PIP_URL = 'https://bootstrap.pypa.io/get-pip.py'
+VC_REDIST_URL = 'https://aka.ms/vs/16/release/vc_redist.x64.exe'
 
 if len(sys.argv) < 3:
     sys.exit('Usage: ./{} install_dir storage_dir'.format(__file__.split('.')[0]))
@@ -28,26 +29,28 @@ make_dir(PYTHON_DIR)
 PTH_PATH = os.path.join(PYTHON_DIR, 'python37._pth')
 
 
-def download_file(url, dst='./'):
+def download_file(url):
     filename = url.split('/')[-1]
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
-        with open(os.path.join(dst, filename), 'wb') as f:
+        with open(filename, 'wb') as f:
             for chunk in r.iter_content(chunk_size=8192): 
                 f.write(chunk)
     return filename
 
 
-# download & unzip python
-python_zip_filename = download_file(PY_EMBED_URL)
-with zipfile.ZipFile(python_zip_filename, 'r') as z:
+print('installing vc_redist')
+vc_redist_filename = download_file(VC_REDIST_URL)
+os.system('{} /install /quiet /norestart'.format(vc_redist_filename))
+os.remove(vc_redist_filename)
+
+print('installing python')
+py_embed_filename = download_file(PY_EMBED_URL)
+with zipfile.ZipFile(py_embed_filename, 'r') as z:
     z.extractall(PYTHON_DIR)
+os.remove(py_embed_filename)
 
-# remove python.zip
-os.remove(python_zip_filename)
-
-# the following two file writes fix importing issues
-# specific to "embedded python"
+print('fixing imports')
 
 with open(PTH_PATH, 'a') as f:
     f.write('\nimport site')
@@ -55,22 +58,22 @@ with open(PTH_PATH, 'a') as f:
 with open(os.path.join(PYTHON_DIR, 'sitecustomize.py'), 'w') as f:
     f.write('import sys; sys.path.insert(0, "")')
 
-# download get-pip.py
+PYTHON_EXE = os.path.join(PYTHON_DIR, 'python.exe')
+
 get_pip_filename = download_file(GET_PIP_URL)
-get_pip_path = os.path.abspath(get_pip_filename)
-
-# setup environment (pip & mindsdb_server & mindsdb_native)
-try:
-    os.system('setupenv.bat {} {}'.format(PYTHON_DIR, get_pip_path))
-except Exception as e:
-    sys.exit(e)
-
-# remove get-pip.py
+os.system('{} {} --no-warn-script-location'.format(PYTHON_EXE, get_pip_filename))    
 os.remove(get_pip_filename)
 
-# create run_server.bat
+LIBS = ['lightwood', 'mindsdb', 'mindsdb_server']
+
+os.system('{} -m pip install torch==1.5.0+cpu torchvision==0.6.0+cpu -f https://download.pytorch.org/whl/torch_stable.html --no-warn-script-location'.format(PYTHON_EXE))
+
+for lib in LIBS:
+    os.system('{} -m pip install {} --no-warn-script-location'.format(PYTHON_EXE, lib))
+
+print('generating run_server.bat')
 with open(os.path.join(INSTALL_DIR, 'run_server.bat'), 'w') as f:
-    cmds = ['set path={0};{0}/Scripts;{0}/Lib/site-packages'.format(PYTHON_DIR),
-            'set MINDSDB_STORAGE_PATH={}'.format(STORAGE_DIR),
-            'python -m mindsdb_server']
-    f.write('\n'.join(cmds))
+    lines = ['{} -m pip install {} --upgrade --no-warn-script-location'.format(PYTHON_EXE, lib) for lib in LIBS]
+    lines.append('set MINDSDB_STORAGE_PATH={}'.format(STORAGE_DIR))
+    lines.append('{} -m mindsdb_server'.format(PYTHON_EXE))
+    f.write('\n'.join(lines))
