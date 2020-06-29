b3831c53b6f81f979f9c28ed477ca2ae8481634e
George3d6 <george@cerebralab.com>
2020-06-29 13:39:34 +0300

    fixed windows installers

--- install_scripts/install_venv_win.bat
+++ install_scripts/install_venv_win.bat
@@ -10,4 +10,7 @@ env\Scripts\pip.exe install --upgrade pip
 env\Scripts\pip.exe install mindsdb --no-cache-dir
 
 echo import runpy > run_mindsdb_server.py
-echo runpy.run_module('mindsdb') >> run_mindsdb_server.py
+echo import torch.multiprocessing as mp >> run_mindsdb_server.py
+echo if __name__ == '__main__': >> run_mindsdb_server.py
+echo.    mp.freeze_support() >> run_mindsdb_server.py
+echo.    runpy.run_module('mindsdb',run_name='__main__') >> run_mindsdb_server.py
